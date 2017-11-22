14e912b387bc2bcc98c8ae0c6d00bed52c57edb8
Richik SC <sc.richik@gmail.com>
2017-11-22 14:44:05 -0600

    add sass compile script

---
+++ compile-sass.sh
@@ -0,0 +1,4 @@
+sass ./css/scss/styles.scss ./css/styles.css
+sass ./css/scss/spacex-launch.scss ./src/spacex-launch/spacex-launch.css
+sass ./css/scss/launch-vehicle.scss ./src/launch-vehicle-elements/launch-vehicle/launch-vehicle.css
+sass ./css/scss/launch-timeline-app.scss ./src/launch-timeline-app/launch-timeline-app.css
